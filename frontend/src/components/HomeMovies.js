1654e6b062457fa214974f7c31ae0bf184612091
Ian Pedro <ianpedro7@gmail.com>
2022-06-20 00:02:08 -0300

    INTEGRADED-API-DATA
    There´s  still a issue for use the instance at the image src

--- frontend/src/components/HomeMovies.js
+++ frontend/src/components/HomeMovies.js
@@ -3,6 +3,15 @@ import React, { Component } from "react";
 export default class HomeMovies extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      movieList: [],
+      activeItem:{
+        id:null,
+        title:'',
+        thumb:'',
+        description:'',
+      },
+    }
     this.fetchMovies = this.fetchMovies.bind(this)
   }
 
@@ -16,19 +25,29 @@ export default class HomeMovies extends Component {
     fetch('http://127.0.0.1:8000/api/movie-list/')
     .then(response => response.json())
     .then(data => 
-        console.log('Data:', data)
+      this.setState({
+        movieList: data
+      })
       )
   }
 
   render() {
+    var movies = this.state.movieList
     return (
-      <div class='py-10'>
+      <div class="py-10">
         <h1>Home Filmes</h1>
         <hr/>
-
+        {movies.map(function(movie, index){
+          return(
+            <div key={index}>
+              <p>{movie.title}</p>
+              <p>{movie.description}</p>
+              <img src={movie.thumb.url}/>
+            </div>
+          )
+        })}
       </div>
     );
   }
 }
 
-;
